437e63eee5edc4a832ca5523457af120cbd7f42d
Koder95 <kammul@outlook.com>
2023-07-09 01:43:06 +0200

    Update Main.java

--- src/main/java/pl/koder95/intencje/Main.java
+++ src/main/java/pl/koder95/intencje/Main.java
@@ -1,12 +1,25 @@
 package pl.koder95.intencje;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
 import pl.koder95.intencje.core.cli.CL;
 
-public class Main {
+public class Main extends Application {
 
     public static final String APP_NAME = "Intencje mszalne";
 
+    @Override
+    public void init() throws Exception {
+        super.init();
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.show();
+    }
+
     public static void main(String[] args) {
         CL.capture(args).service(new ConfigCLI());
+        launch(args);
     }
 }
