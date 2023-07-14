b24a78ea03b6621b10f8f1003d671e602a5578a8
Koder95 <kammul@outlook.com>
2023-07-14 02:23:51 +0200

    Uzupełniono brakujące importowania

--- src/main/java/pl/koder95/intencje/gui/MainFrame.java
+++ src/main/java/pl/koder95/intencje/gui/MainFrame.java
@@ -1,5 +1,8 @@
 package pl.koder95.intencje.gui;
 
+import javax.swing.JFrame;
+import java.awt.HeadlessException;
+
 import static pl.koder95.intencje.Main.APP_HEADER;
 
 public class MainFrame extends JFrame {
