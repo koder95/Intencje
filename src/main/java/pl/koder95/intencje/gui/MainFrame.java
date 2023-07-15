e116e47468f61d509925a2209d5c5d1ef42558ce
Koder95 <kammul@outlook.com>
2023-07-15 20:43:39 +0200

    Zdefiniowanie metody initComponents

--- src/main/java/pl/koder95/intencje/gui/MainFrame.java
+++ src/main/java/pl/koder95/intencje/gui/MainFrame.java
@@ -1,6 +1,8 @@
 package pl.koder95.intencje.gui;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
 import java.awt.HeadlessException;
 
 import static pl.koder95.intencje.Main.APP_HEADER;
@@ -21,5 +23,33 @@ public class MainFrame extends JFrame {
     }
 
     private void initComponents() {
+        JPanel contentPane = new JPanel(new BorderLayout(5, 5));
+        setContentPane(contentPane);
+
+        contentPane.add(createHeader(), BorderLayout.NORTH);
+        contentPane.add(createLeftSide(), BorderLayout.WEST);
+        contentPane.add(createMainPane(), BorderLayout.CENTER);
+        contentPane.add(createRightSide(), BorderLayout.EAST);
+        contentPane.add(createFooter(), BorderLayout.SOUTH);
+    }
+
+    private JPanel createRightSide() {
+        return null;
+    }
+
+    private JPanel createLeftSide() {
+        return null;
+    }
+
+    private JPanel createHeader() {
+        return null;
+    }
+
+    private JPanel createMainPane() {
+        return null;
+    }
+
+    private JPanel createFooter() {
+        return null;
     }
 }
